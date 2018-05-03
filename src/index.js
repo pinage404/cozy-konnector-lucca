9f675c4f63330679b70613ca6a93bd05127c112a
Sébastien Duval <sduval@smartadserver.com>
2018-05-03 17:47:07 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,49 +1,120 @@
 const {
   BaseKonnector,
+  log,
   requestFactory,
   saveFiles,
-  addData
+  errors
 } = require('cozy-konnector-libs')
-const request = requestFactory({ cheerio: true })
+const request = requestFactory({
+  //cheerio: true
+  debug: true,
+  json: true,
+  jar: true
+})
+const moment = require('moment')
 
-const baseUrl = 'http://books.toscrape.com'
+const baseStartUrl = 'https://www.lucca.fr'
+let baseWorkspaceUrl
+let userId
 
 module.exports = new BaseKonnector(start)
-
 // The start function is run by the BaseKonnector instance only when it got all the account
 // information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
 // the account information come from ./konnector-dev-config.json file
-function start(fields) {
-  // The BaseKonnector instance expects a Promise as return of the function
-  return request(`${baseUrl}/index.html`).then($ => {
-    // cheerio (https://cheerio.js.org/) uses the same api as jQuery (http://jquery.com/)
-    // here I do an Array.from to convert the cheerio fake array to a real js array.
-    const entries = Array.from($('article')).map(article =>
-      parseArticle($, article)
-    )
-    return addData(entries, 'io.cozy.books').then(() =>
-      saveFiles(entries, fields)
-    )
-  })
+async function start(fields) {
+  await logIn(fields)
+  const payslips = await getPayslips()
+  await saveFiles(payslips, fields)
+  // .then(convertPayrollsToCozy)
+  // .then(documents => saveFiles(documents, fields));
 }
 
-// The goal of this function is to parse a html page wrapped by a cheerio instance // and return an array of js objects which will be saved to the cozy by addData (https://github.com/cozy/cozy-konnector-libs/blob/master/docs/api.md#module_addData)
-// and saveFiles (https://github.com/cozy/cozy-konnector-libs/blob/master/docs/api.md#savefiles)
-function parseArticle($, article) {
-  const $article = $(article)
-  const title = $article.find('h3 a').attr('title')
-  return {
-    title,
-    price: normalizePrice($article.find('.price_color').text()),
-    url: `${baseUrl}/${$article.find('h3 a').attr('href')}`,
-    // when it finds a fileurl attribute, saveFiles will save this file to the cozy with a filename
-    // name
-    fileurl: `${baseUrl}/${$article.find('img').attr('src')}`,
-    filename: `${title}.jpg`
+async function logIn(fields) {
+  log('info', 'First log in to get user company details...')
+  try {
+    const details = await request({
+      uri: baseStartUrl + '/login/ws-auth-service/wsauth.service.php',
+      method: 'POST',
+      form: {
+        mail: fields.login,
+        password: fields.password,
+        request: 'emailpass'
+      }
+    })
+    baseWorkspaceUrl = details.data.user.instance.href
+    userId = details.data.user.login
+
+    // const uri =
+    //   "https://auth.payfit.com/updateCurrentCompany?application=hr-apps/user&companyId=";
+
+    // return request({
+    //   uri: `${uri}${companyId}&customApp=false&employeeId=${employeeId}&holdingId&idToken=${idToken}&origin=https://app.payfit.com`
+    // });
+  } catch (err) {
+    if (
+      err.statusCode === 404 &&
+      err.error.Message &&
+      err.error.Message.includes('Wrong password')
+    ) {
+      throw new Error(errors.LOGIN_FAILED)
+    } else {
+      throw new Error(errors.VENDOR_DOWN)
+    }
+  }
+
+  log('info', 'Log in with company URL')
+  try {
+    await request({
+      uri: baseWorkspaceUrl + '/login',
+      method: 'POST',
+      form: {
+        login: userId,
+        Password: fields.password,
+        PersistentCookie: true
+      }
+    })
+    // const uri =
+    //   "https://auth.payfit.com/updateCurrentCompany?application=hr-apps/user&companyId=";
+
+    // return request({
+    //   uri: `${uri}${companyId}&customApp=false&employeeId=${employeeId}&holdingId&idToken=${idToken}&origin=https://app.payfit.com`
+    // });
+  } catch (err) {
+    if (
+      err.statusCode === 400 &&
+      err.error &&
+      err.error.includes('Vos identifiants sont erron&#233;s')
+    ) {
+      throw new Error(errors.LOGIN_FAILED)
+    }
   }
 }
 
-// convert a price string to a float
-function normalizePrice(price) {
-  return parseFloat(price.trim().replace('£', ''))
+async function getPayslips() {
+  log('info', 'Get Pagga userId')
+  const userDetails = await request({
+    uri: baseWorkspaceUrl + '/api/v3/users/me?fields=id',
+    method: 'GET'
+  })
+  const paggaUserId = userDetails.data.id
+
+  log('info', 'Get Payslips')
+  const payslipsInfos = await request({
+    uri:
+      baseWorkspaceUrl +
+      '/api/v3/payslips?fields=id,import[endDate]&orderby=import.endDate,desc,import.startDate,desc,import.creationDate,desc&ownerID=' +
+      paggaUserId,
+    method: 'GET'
+  })
+
+  return payslipsInfos.data.items.map(function(payslip) {
+    const url = baseWorkspaceUrl + '/pagga/download/' + payslip.id
+    const date = moment(payslip.import.endDate, moment.ISO_8601)
+    const filename = date.format('YYYY_MM') + '.pdf'
+
+    return {
+      fileurl: url,
+      filename
+    }
+  })
 }
